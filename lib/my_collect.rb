6728c4e6e12e30117acd671dd29b307b70d1ba52
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-12 13:08:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,6 +2,7 @@ def my_collect(your_collection)
   i = 0
   while i < your_collection.size
     yield your_collection[i]
+    new_collection 
     i += 1
   end
 end
@@ -10,3 +11,4 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
 end
+puts array
